837c593bb832581fa6adf91e11633dbf2c19ef64
Marwan Darwish <marwandarwish9@gmail.com>
2024-10-19 01:52:08 +0300

    make it better

--- carzone/settings.py
+++ carzone/settings.py
@@ -57,8 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'carzone.urls'
